3542942a8277772739c8e21415ad38fce73f3d86
HBazai <bazaee.hassan@gmail.com>
2022-01-09 12:29:04 +0330

    Base.html and urls has been configured

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,6 +1,6 @@
 from datetime import timezone
-from django.shortcuts import redirect, render
-from django.views.generic.dates import timezone_today
+from django.shortcuts import redirect, render, get_object_or_404
+from django.utils import timezone
 from blog_app.models import Post, Comments
 from blog_app.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
@@ -53,3 +53,43 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__insull=True).order_by('created_date')
+
+
+### Functions ###
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
+
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.POST = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form':form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comments, pk = pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comments, pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+
